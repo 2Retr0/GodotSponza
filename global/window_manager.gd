1e5823ae1202682f0660b7979714e05fa6e7b520
Ethan Truong <18603664+2Retr0@users.noreply.github.com>
2025-02-11 12:01:28 -0800

    Change light bake mode to 'static'

--- global/window_manager.gd
+++ global/window_manager.gd
@@ -5,7 +5,7 @@ const WINDOW_SCALE = 0.6
 func _ready() -> void:
 	var window := get_window()
 	if window.is_embedded() or Engine.is_embedded_in_editor(): return
-	
+
 	# Hack to visually hide window until size is changed to fit a percentage.
 	# of the screen resolution.
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
